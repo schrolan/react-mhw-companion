052140b8a6c1e96af5cd6279e8a934ab0a17749a
schrolan <schrolan000@gmail.com>
2024-10-27 21:31:59 -0500

    got the app running locally

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,31 +1,23 @@
-const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = process.env.MONGODB_URI || "mongodb+srv://root:root@cluster0.5jsp7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mhw-companion')
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+});
 
-module.exports = mongoose.connection
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to MongoDB!")
+        return client;
+    } catch (err) {
+        console.log("Failed to connect to MongoDB!", err);
+        throw err;
+    }
+}
 
-
-//This will be for when it is deployed
-// const { MongoClient, ServerApiVersion } = require('mongodb')
-// const uri = process.env.MONGODB_URI || "mongodb+srv://root:root@cluster0.5jsp7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true
-//     }
-// });
-
-// async function connectDB() {
-//     try {
-//         await client.connect();
-//         console.log("Successfully connected to MongoDB!")
-//         return client;
-//     } catch (err) {
-//         console.log("Failed to connect to MongoDB!", err);
-//         throw err;
-//     }
-// }
-
-// module.exports = { connectDB, client };
+module.exports = { connectDB, client };
--- server/server.js
+++ server/server.js
@@ -7,26 +7,26 @@ const typeDefs = require('./schemas/typeDefs');
 const resolvers = require('./schemas/resolvers');
 const { authMiddleware } = require('./utils/auth');
 
-//Connects us to the port
+// Connect to the port
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-//Middleware for parsing data
+// Middleware for parsing data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Serve static files from the React app
+// Serve static files from the React app
 if (process.env.NODE_ENV === 'production') {
+    console.log('Serving static files from client/build');
     app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 }
 
-//This connects us to MongoDB
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mhw-companion', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
-  .then(async () => {
+})
+.then(async () => {
     console.log('MongoDB connected');
   
     // Start Apollo Server
@@ -48,8 +48,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mhw-compa
       console.log(`Express server listening on http://localhost:${PORT}`);
       console.log(`Apollo GraphQL playground available at http://localhost:${PORT}/graphql`);
     });
-  })
-  .catch(err => {
+})
+.catch(err => {
     console.error('MongoDB connection error:', err);
-  });
-  
+});  
